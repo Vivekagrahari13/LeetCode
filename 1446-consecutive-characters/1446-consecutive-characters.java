8496e970ad2ff430f5b4b04fceaa9718595debb6
Vivek Agrahari <91648402+Vivekagrahari13@users.noreply.github.com>
2023-01-26 00:10:11 +0530

    Time: 1 ms (100.00%), Space: 42.5 MB (34.46%) - LeetHub

---
+++ 1446-consecutive-characters/1446-consecutive-characters.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int maxPower(String s) 
+    {
+        char [] ch=s.toCharArray();
+        //Arrays.sort(ch);
+        int max=1;
+        int curr=1;
+        for(int i=1;i<s.length();i++)
+        {
+            if(ch[i]==ch[i-1])
+            {
+                curr++;
+                max=Math.max(curr,max);
+            }
+            else
+            {
+                curr=1;
+            }
+        }
+        return max;
+    }
+}
