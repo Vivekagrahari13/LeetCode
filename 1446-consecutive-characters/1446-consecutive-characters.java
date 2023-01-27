19f0c912e55a230dbcb0685efdd0cda8ac6dd28a
Vivek Agrahari <91648402+Vivekagrahari13@users.noreply.github.com>
2023-01-28 01:23:56 +0530

    Time: 1 ms (100.00%), Space: 41.1 MB (57.98%) - LeetHub

--- 1446-consecutive-characters/1446-consecutive-characters.java
+++ 1446-consecutive-characters/1446-consecutive-characters.java
@@ -1,13 +1,14 @@
 class Solution {
     public int maxPower(String s) 
     {
-        char [] ch=s.toCharArray();
+        //char [] ch=s.toCharArray();
         //Arrays.sort(ch);
         int max=1;
         int curr=1;
         for(int i=1;i<s.length();i++)
         {
-            if(ch[i]==ch[i-1])
+            // if(ch[i]==ch[i-1])
+            if(s.charAt(i)==s.charAt(i-1))
             {
                 curr++;
                 max=Math.max(curr,max);
