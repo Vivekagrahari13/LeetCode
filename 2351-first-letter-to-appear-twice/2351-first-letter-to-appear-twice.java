aad0915ef47b4839e745e76cdf2d52491d1bf4d6
Vivek Agrahari <91648402+Vivekagrahari13@users.noreply.github.com>
2023-02-07 20:59:40 +0530

    Time: 1 ms (28.94%), Space: 40.3 MB (70.58%) - LeetHub

--- 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.java
+++ 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.java
@@ -3,22 +3,15 @@ class Solution
     public char repeatedCharacter(String s) 
     {
        LinkedHashMap<Character ,Integer> map=new LinkedHashMap<>();
-        char ch[]=s.toCharArray();
-        for(Character x:ch)
+        //char ch[]=s.toCharArray();
+        for(int i=0;i<s.length();i++)
         {
-            map.put(x,map.getOrDefault(x,0)+1);
+            map.put(s.charAt(i),map.getOrDefault(s.charAt(i),0)+1);
             if(map.containsValue(2))
             {
-                return x;
+                return s.charAt(i);
             }
-        }
-        /*for(Map.Entry<Character,Integer> e:map.entrySet())
-        {
-            if(e.getValue()==2)
-            {
-                return e.getKey();
-            }
-        }*/
+        } 
         return ' ';
     }
 }
