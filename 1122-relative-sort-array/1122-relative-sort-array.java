ff8ef817aef6638a2392b0f4807255ccb946a33a
Vivek Agrahari <91648402+Vivekagrahari13@users.noreply.github.com>
2023-02-10 14:19:16 +0530

    Time: 2 ms (68.90%), Space: 42.5 MB (28.52%) - LeetHub

--- 1122-relative-sort-array/1122-relative-sort-array.java
+++ 1122-relative-sort-array/1122-relative-sort-array.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[] relativeSortArray(int[] arr1, int[] arr2) {
-        int[] flag = new int[1001];
+        /*int[] flag = new int[1001];
         for(int n : arr1) 
         {
         flag[n]++;
@@ -14,6 +14,37 @@ class Solution {
         for(int n = 0; n < flag.length; n++) {
             while(flag[n]-- > 0) {
                 arr1[i++] = n;
+            }
+        }
+        return arr1; */
+        Arrays.sort(arr1);
+        LinkedHashMap<Integer, Integer> map=new LinkedHashMap<>();
+        for(int a:arr1)
+        {
+            map.put(a,map.getOrDefault(a,0)+1);
+        }
+        int k=0;
+        for(int b:arr2)
+        {
+            if(map.containsKey(b)){
+                int i=map.get(b);
+            while(i>0)
+            {
+               arr1[k]=b;
+                i--;
+                k++;
+            }
+            }
+            map.remove(b);
+        }
+        for(Map.Entry<Integer,Integer> e:map.entrySet())
+        {
+            int i=e.getValue();
+            while(i>0)
+            {
+                arr1[k]=e.getKey();
+                i--;
+                k++;
             }
         }
         return arr1;
