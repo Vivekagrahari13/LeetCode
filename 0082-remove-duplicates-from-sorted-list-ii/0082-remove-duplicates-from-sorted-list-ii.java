26405f1a8e8c82d10122f424d1686584d576f7ed
Vivek Agrahari <91648402+Vivekagrahari13@users.noreply.github.com>
2023-03-20 12:30:17 +0530

    Time: 3 ms (9.55%), Space: 42.5 MB (19.47%) - LeetHub

--- 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.java
+++ 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.java
@@ -16,42 +16,22 @@ class Solution {
             return head;
         }
         
-        HashMap<Integer, Integer> map=new HashMap<>();
+        Map<Integer, Integer> map=new LinkedHashMap<>();
         for(ListNode curr=head;curr!=null;curr=curr.next)
         {
             map.put(curr.val,map.getOrDefault(curr.val,0)+1);
         }
-        List<Integer> list=new ArrayList<>();
+        ListNode new_head=new ListNode(0);
+        ListNode flag=new_head;
         for(Map.Entry<Integer,Integer> e:map.entrySet())
         {
-            //System.out.println(e.getValue());
             if(e.getValue()==1)
             {
-                list.add(e.getKey());
+                    ListNode temp=new ListNode(e.getKey());
+                    flag.next=temp;
+                    flag=flag.next;
             }
         }
-        if(list.isEmpty())
-        {
-            return null;
-        }
-        ListNode curr_node=head, final_head=head;
-        boolean flag =true;
-        while(curr_node!=null)
-        {
-            if(list.contains(curr_node.val) && flag==true)
-            {
-                head=curr_node;
-                final_head=curr_node;
-                flag=false;
-            }
-            else if(list.contains(curr_node.val) && flag==false)
-            {
-                head.next=curr_node;
-                head=head.next;
-            }
-            curr_node=curr_node.next;
-        }
-         head.next=null;
-         return final_head;
+        return new_head.next;
 }
 }
