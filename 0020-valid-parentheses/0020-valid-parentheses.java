f68b5a733078ecb6b6e00ed07c424f8d6a03c524
Vivek Agrahari <91648402+Vivekagrahari13@users.noreply.github.com>
2023-04-11 22:51:43 +0530

    Time: 2 ms (79.82%), Space: 40.5 MB (53.20%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,36 @@
+class Solution {
+    public boolean isValid(String s)
+    {
+        Deque<Character>stack=new ArrayDeque<>();
+		int i=0;
+		while(i<s.length())
+		{
+			char x=s.charAt(i);
+			if(x=='(' || x=='['  || x=='{')
+			{
+				stack.push(x);
+			}
+			else
+			{
+				if( stack.isEmpty()==true)
+				{
+					return false;
+				}
+				else if(matchBrace(stack.peek(),x)==false)
+				{
+					return false;
+				}
+				else
+				{
+					stack.pop();
+				}
+			}
+			i++;
+		}
+		return (stack.isEmpty()==true);
+    }
+    static boolean matchBrace(char a, char b)
+	{
+		return (a=='(' && b==')')|| (a=='{'&& b=='}') ||( a=='[' && b==']');
+	}
+}
