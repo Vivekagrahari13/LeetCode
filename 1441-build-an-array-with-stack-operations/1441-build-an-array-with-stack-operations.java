d20e9c564be4728f1447e0445189d0113221bce3
Vivek Agrahari <91648402+Vivekagrahari13@users.noreply.github.com>
2023-04-12 02:10:02 +0530

    Time: 1 ms (26.53%), Space: 42.7 MB (54.70%) - LeetHub

---
+++ 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.java
@@ -0,0 +1,39 @@
+class Solution {
+    public List<String> buildArray(int[] target, int n) {
+        List<String> list=new ArrayList<>();
+        Deque<Integer> stack=new ArrayDeque<>();
+        int i=1,len=target.length;
+        int size=0;
+        while(i<=n)
+        {
+            if(stack.isEmpty())
+            {
+                stack.push(i);
+                list.add("Push");
+                size++;
+            }
+            else
+            {
+                if(target[len-1]==stack.peek())
+                {
+                    break;
+                }
+                else if(size==len || target[size-1]!=stack.peek())
+                {
+                    stack.pop();
+                    list.add("Pop");
+                    size--;
+                    i--;
+                }
+                else
+                {  
+                stack.push(i);
+                list.add("Push");
+                size++;
+                }
+            }
+            i++;
+        }
+        return list;
+    }
+}
