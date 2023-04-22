ac9dada4f8622a8ab865e4db698f9a358f53ebd5
Vivek Agrahari <91648402+Vivekagrahari13@users.noreply.github.com>
2023-04-22 08:07:39 +0530

    Time: 23 ms (90.99%), Space: 56.8 MB (46.63%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -1,34 +1,26 @@
 class Solution {
     public int largestRectangleArea(int[] heights) {
-        int area=0,n=heights.length;
+        int n=heights.length;
         Deque<Integer> stack=new ArrayDeque<>();
-        int ps[]=new int[n];
-        int ns[]=new int[n];
+        int area=0;
         for(int i=0;i<n;i++)
-        {
-            while(!stack.isEmpty()&& heights[stack.peek()]>=heights[i])
-            {
-                stack.pop();
-            }
-            ps[i]=stack.isEmpty()?-1:stack.peek();
-            stack.push(i);
-        }
-        stack.clear();
-        for(int i=n-1;i>=0;i--)
         {
             while(!stack.isEmpty() && heights[stack.peek()]>=heights[i])
             {
+                int top=stack.peek();
                 stack.pop();
+                int res=(heights[top])*(stack.isEmpty()?i:(i-stack.peek()-1));
+                area=Math.max(area,res);
             }
-            ns[i]=stack.isEmpty()?n:stack.peek();
             stack.push(i);
         }
-        for(int i=0;i<n;i++)
-        {
-            int curr=heights[i];
-            curr=curr+curr*(i-ps[i]-1)+curr*(ns[i]-i-1);
-            area=Math.max(curr,area);
-        }
+        while(!stack.isEmpty() )
+            {
+                int top=stack.peek();
+                stack.pop();
+                int res=(heights[top])*(stack.isEmpty()?n:(n-stack.peek()-1));
+                area=Math.max(area,res);
+            }
         return area;
     }
 }
