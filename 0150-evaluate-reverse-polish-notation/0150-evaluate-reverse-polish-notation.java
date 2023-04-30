abc5e8d807a2d25bb7cbd37662a1a387ee2487fe
Vivek Agrahari <91648402+Vivekagrahari13@users.noreply.github.com>
2023-05-01 01:58:12 +0530

    Time: 8 ms (36.58%), Space: 42.6 MB (35.19%) - LeetHub

---
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int evalRPN(String[] tokens) {
+        Deque<Integer> stack=new ArrayDeque<>();
+        int i=0;
+        while(i<tokens.length)
+        {
+            char x=tokens[i].charAt(tokens[i].length()-1);
+            if(isOperand(x))
+            {
+                int num=Integer.parseInt(tokens[i]);
+                stack.push(Integer.parseInt(num+""));
+            }
+            else 
+            {
+                int op1=stack.pop();
+                int op2=stack.pop();
+                
+                 if(x=='*')
+                {
+                    stack.push(op2*op1);
+                }
+                else if(x=='/')
+                {
+                    stack.push(op2/op1);
+                }
+                else if(x=='+')
+                {
+                    stack.push(op2+op1);
+                }
+                else if(x=='-')
+                {
+                    stack.push(op2-op1);
+                }
+            }
+            i++;
+        }
+        return stack.peek();
+    }
+    public boolean isOperand(char num)
+    {
+        return Character.isDigit(num);
+    }
+}
