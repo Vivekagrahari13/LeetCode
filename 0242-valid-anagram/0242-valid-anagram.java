5378410a64c3ce59c758066769e3f16ee5e5b8d1
Vivek Agrahari <91648402+Vivekagrahari13@users.noreply.github.com>
2023-06-30 19:54:54 +0530

    Time: 3 ms (93.76%), Space: 41.8 MB (97.92%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,14 +1,19 @@
 class Solution {
+    static final int CHAR=256;
     public boolean isAnagram(String s, String t) {
         if(s.length()!=t.length())
             return false;
-        char a[]=s.toCharArray();
-        char b[]=t.toCharArray();
-        Arrays.sort(a);
-        Arrays.sort(b);
-        s=new String(a);
-        t=new String(b);
-        return s.equals(t);
-        
+        int ch[]=new int[CHAR];
+        for(int i=0;i<s.length();i++)
+        {
+            ch[s.charAt(i)]++;
+            ch[t.charAt(i)]--;
+        }
+        for(int i=0;i<CHAR;i++)
+        {
+            if(ch[i]!=0)
+                return false;
+        }
+        return true;
     }
 }
