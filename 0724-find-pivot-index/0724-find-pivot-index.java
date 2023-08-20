af3054c3ef79d2e7c18203e16c66f220f63d51d7
Vivek Agrahari <91648402+Vivekagrahari13@users.noreply.github.com>
2023-08-20 13:23:49 +0530

    Time: 1 ms (99.50%), Space: 44.5 MB (20.65%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -1,26 +1,15 @@
-//By Vivek Agrahari
-// using Prefix Sum
-class Solution
-{
-    public int pivotIndex(int[] nums)
-    {
-        int a_sum=0;
-        for(int i=0;i<nums.length;i++)
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int n=nums.length, arrSum=0;
+        for(int i=0;i<n;i++)
+            arrSum+=nums[i];
+        int leftSum=0;
+        for(int i=0;i<n;i++)
         {
-            a_sum+=nums[i];
-        }
-        int left_sum=0;
-        for(int i=0;i<nums.length;i++)
-        {
-            if(left_sum==a_sum-nums[i])
-            {
+            if(leftSum==arrSum-nums[i])
                 return i;
-            }
-            else
-            {
-                left_sum+=nums[i];
-                a_sum=a_sum-nums[i];
-            }
+            leftSum+=nums[i];
+            arrSum-=nums[i];
         }
         return -1;
     }
